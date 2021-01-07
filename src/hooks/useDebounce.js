e6f50997517165d38ca129206cf5ada89b674b1e
Sam Pilehvarian <samp.codes@gmail.com>
2021-01-07 11:31:33 -0500

    Refactor the component's code

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,15 +1,30 @@
 import { useState, useEffect } from 'react';
 
-export default function useDebounce(term, ms) {
-	const [debounced, setDebounced] = useState('');
+/**
+ * @function useDebounce - custom hook that will debounce a value from a text input
+ * @param {string} value - the string value from the text input
+ * @param {number} delay - the delay in ms until the value is set
+ * @returns {null/string} - returns null if value is empty string or the string value
+ */
+const useDebounce = (value, delay) => {
+	const [debouncedValue, setDebouncedValue] = useState(value);
 
 	useEffect(() => {
-		const timeout = setTimeout(() => {
-			setDebounced(term);
-		}, ms);
+		// Set debouncedValue to value (passed in) after the specified delay
+		const handler = setTimeout(() => {
+			setDebouncedValue(value);
+		}, delay);
 
-		return () => clearTimeout(timeout);
-	}, [term, ms]);
+		return () => {
+			clearTimeout(handler);
+		};
+	}, [value, delay]);
 
-	return debounced;
-}
+	if (value === '') {
+		return null;
+	}
+
+	return debouncedValue;
+};
+
+export default useDebounce;
