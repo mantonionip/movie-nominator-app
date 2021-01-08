bc3ad606e7afcc973abfe12b1c187614cd2107bb
Sam Pilehvarian <samp.codes@gmail.com>
2021-01-08 10:59:39 -0500

    Add clear search bar functionality to the form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import Cross from '../assets/times-solid.svg';
 
 function SearchBar(props) {
 	const handleInput = (event) => props.setInputValue(event.target.value);
+	const clearSearch = () => props.setInputValue('');
 
 	return (
 		<div className="search-bar">
 			<div className="search-bar__container">
-				<p className="search-bar__title">Search Title</p>
 				<form
 					className="search-bar__form"
 					onSubmit={(e) => e.preventDefault()}
 					autoComplete="off"
 				>
-					<input
-						type="search"
-						className="search-bar__input"
-						placeholder="e.g. Some like it hot..."
-						value={props.inputValue}
-						onChange={handleInput}
-					/>
+					<label
+						id="SearchBarField"
+						htmlFor="SearchBarFieldForm"
+						className="search-bar__title"
+					>
+						Search Title
+					</label>
+					<div className="search-bar__input-container">
+						<input
+							id="SearchBarFieldForm"
+							type="search"
+							className="search-bar__input"
+							placeholder="e.g. Some like it hot..."
+							value={props.inputValue}
+							onChange={handleInput}
+						/>
+						{props.inputValue && (
+							<div
+								className="search-bar__clear"
+								onClick={clearSearch}
+							>
+								<img
+									id="crossIcon"
+									className="search-bar__icon"
+									src={Cross}
+									alt="Close icon."
+								/>
+							</div>
+						)}
+					</div>
 				</form>
 			</div>
 		</div>
